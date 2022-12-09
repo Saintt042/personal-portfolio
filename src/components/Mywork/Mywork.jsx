ebcd2cc09891646799d7c4969912d97cefbdfa0d
Alinwachukwu Michael <saintmichaell42@gmail.com>
2022-12-09 20:03:26 +0100

    modified files

--- src/components/Mywork/Mywork.jsx
+++ src/components/Mywork/Mywork.jsx
@@ -30,7 +30,7 @@ const Mywork = () => {
     // ]
   return (
     <Container>
-         <h1> PROJECTS </h1>
+         <h1 className="pro"> PROJECTS </h1>
          <p>These are some interesting and fun projects i have worked on</p>
         {/* <section className='projects'>
 				{projectList.map((item, indx) => (
@@ -38,6 +38,28 @@ const Mywork = () => {
 				))}
         
 			</section> */}
+
+      <div className="links">
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://certificate.terrahq.co/75012M25I16RUE14"
+          >SideHustle Front-end web development Certificate
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.udemy.com/certificate/UC-e5edf5b8-553d-40bf-b4d4-633fc49f49d8/"
+          >Udemy Front-end web development Certificate
+          </a>
+        </li>
+        </ul>
+      </div>
     </Container>
   )
 }
--- src/components/Mywork/MyworkStyle.js
+++ src/components/Mywork/MyworkStyle.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 
 export const Container = styled.section`
-	padding: 40px 7% 20px 7%;
-	h2 {
+	
+	.pro {
 		text-align: center;
 		font-size: clamp(1.5rem, 4vw, 2.8rem);
 		margin-bottom: 1.5rem;
